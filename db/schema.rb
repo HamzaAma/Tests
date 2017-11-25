522db254faf2a05e1cbcbce4dd1686ec3f12d352
hamza.hm2222 <hamza.hm2222@gmail.com>
2017-11-25 05:53:26 -0500

    student

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171125084508) do
+ActiveRecord::Schema.define(version: 20171125105050) do
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.date     "birthday"
+    t.string   "surname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "towns", force: true do |t|
     t.string   "name"
